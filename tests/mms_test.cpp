b829a0e7631c442f4ccd4c6090323daf9357e698
Dmitry Prokoptsev <dprokoptsev@yandex-team.ru>
2014-02-17 22:39:47 +0400

    Unit test infrastructure.

---
+++ tests/mms_test.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE mms_test
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test)
+{
+    BOOST_CHECK_EQUAL(1, 1);
+}
---
+++ tests/stub_test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "Unit tests have been disabled due to lack of build dependencies" >&2
+exit 1
