f3736739ff2cd304be46e1282b1721f6e958c1a2
Dmitry Prokoptsev <dprokoptsev@yandex-team.ru>
2014-02-17 18:42:04 +0400

    Add copyright notices; fix a typo in license file name.

--- include/mms/impl/fwd.h
+++ include/mms/impl/fwd.h
@@ -1,7 +1,30 @@
-// mms -- a memory-mapped storage.
-//
-// See https://github.com/dprokoptsev/mms/wiki/Library-usage
-// for short instruction about its usage.
+
+/*
+ * impl/fwd.h -- forward declarations for all mms containers
+ *
+ * Copyright (c) 2011-2014 Dmitry Prokoptsev <dprokoptsev@yandex-team.ru>
+ *
+ * This file is part of mms, the memory-mapped storage library.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 
 #pragma once
 
@@ -11,11 +34,15 @@
 namespace mms {
 
 class Writer;
+
+template<class P< class K, class V, template<class> class Hash, template<class> class Eq> class unordered_map;
+template<class P< class T, template<class> class Hash, template<class> class Eq> class unordered_set;
 template<class P, class K, class V, template<class> class Cmp = std::less> class map;
 template<class P, class T, template<class> class Cmp = std::less> class set;
 template<class P, class T> class vector;
 template<class P, class T> class optional;
 template<class P> class string;
+
 typedef size_t FormatVersion;
 
 }//namespace mms
